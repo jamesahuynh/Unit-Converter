7e08e6b208798ce25528ae0b744de7fde9fbb7b5
James Huynh <165088389+jamesahuynh@users.noreply.github.com>
2026-04-19 16:25:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ function calculate_temperature() {
     let f = (c * 9/5) + 32;
     document.getElementById("fahrenheit").value = f
 }
-
 function calculate_weight() {
     let kg = document.getElementById("kilo").value;
     let lb = kg * 2.205;
     document.getElementById("pounds").value = lb;
 }
-
 function calculate_distance() {
     let km = document.getElementById("km").value;
     let miles = km / 1.609;
